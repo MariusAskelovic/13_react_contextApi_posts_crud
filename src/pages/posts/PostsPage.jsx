4fcea6e2a0703f40cc994c85511ecb1069f286e3
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-23 23:24:10 +0300

    filter

--- src/pages/posts/PostsPage.jsx
+++ src/pages/posts/PostsPage.jsx
@@ -2,12 +2,15 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import SinglePostLink from '../../components/posts/SinglePostLink';
+import css from './PostsPage.module.css';
 // TODO: turetu buti config.js
 // TODO: use .env file
 const url = 'http://localhost:5000/posts';
 
 export default function PostsPage() {
   const [postsArr, setPostsArr] = useState([]);
+  const [filteredArr, setFilteredArr] = useState([]);
+  const [filterVal, setFilterVal] = useState('');
 
   useEffect(() => {
     // parsisiusti
@@ -17,11 +20,26 @@ export default function PostsPage() {
         console.log('resp ===', resp);
         // irasyti i postsArr
         setPostsArr(resp.data);
+        setFilteredArr(resp.data);
       })
       .catch((error) => {
         console.warn('ivyko klaida:', error);
       });
   }, []);
+
+  function filterArr(event) {
+    const filterVal = event.target.value;
+    setFilterVal(filterVal);
+    console.log('filterVal ===', filterVal);
+    if (filterVal.length > 0) {
+      const newArr = postsArr.filter((obj) =>
+        obj.title.toLowerCase().includes(filterVal.toLowerCase())
+      );
+      setFilteredArr(newArr);
+    } else {
+      setFilteredArr(postsArr);
+    }
+  }
 
   return (
     <div className='container'>
@@ -30,11 +48,16 @@ export default function PostsPage() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
         corporis, reiciendis eius ipsa laborum sed!
       </p>
+      <input
+        type='text'
+        placeholder='filter'
+        className={css.filterInput}
+        onChange={filterArr}
+        value={filterVal}
+      />
       <ul>
-        {postsArr.map((pObj) => (
-          // vietoj li generuoti SinglePostLink
+        {filteredArr.map((pObj) => (
           <li key={pObj.id}>
-            {/* vietoj "5" paduoti posto id */}
             <Link to={'/posts/' + pObj.id}>
               <SinglePostLink title={pObj.title} author={pObj.author} />
             </Link>
