2b1fecb875ee3db739542ef267c861c4406f9cac
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-23 11:51:22 +0300

    get posts

--- src/pages/posts/PostsPage.jsx
+++ src/pages/posts/PostsPage.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// TODO: turetu buti config.js
+// TODO: use .env file
+const url = 'http://localhost:5000/posts';
+
 export default function PostsPage() {
+  const [postsArr, setPostsArr] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log(resp.data);
+        setPostsArr(resp.data);
+      })
+      .catch(console.warn);
+  }, []);
+
   return (
     <div className='container'>
       <h2>PostsPage</h2>
@@ -6,6 +25,11 @@ export default function PostsPage() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique odio
         eveniet illum porro minima est vel corrupti! Nihil, sapiente odit.
       </p>
+      <ul>
+        {postsArr.map((pObj) => (
+          <li key={pObj.id}>{pObj.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
